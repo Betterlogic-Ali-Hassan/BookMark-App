d7897ace524f2c3c642598e586a4b8bcc7e5839a
HassanThePassionate <maqsoodia777@gmail.com>
2025-01-27 12:11:20 +0500

    Update

--- src/components/bookMark/BookMark.tsx
+++ src/components/bookMark/BookMark.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import BookMarkInput from "./BookMarkInput";
 import BookmarkSelect from "./BookMarkSelect";
 import BookMarkBtn from "./BookMarksBtn";
